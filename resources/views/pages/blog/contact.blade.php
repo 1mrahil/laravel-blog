53a1a69583f47463d3ec8552efb299b1f2784b67
Rens van den Brink <rensvandenbrink@gmail.com>
2019-04-18 15:03:02 +0200

    contactformulier is af en werkt

--- resources/views/pages/blog/contact.blade.php
+++ resources/views/pages/blog/contact.blade.php
@@ -4,7 +4,51 @@
 
 
 @section('content')
-<main class="py-5">
+
+<div class="container p-5">
+    <h2 class="text-center pb-5">Heeft u een vraag?</h2>
+ <form method="POST" action="/pages/blog/contact">
+   @csrf
+   <fieldset>
+     <div class="form-group has-warning">
+       <div class="form-row">
+         <div class="col-sm-12">
+           <label class="form-control-label" for="name">Naam*</label>
+           <input name="name" class="form-control form-control-danger" id="name" type="text" value="{{ old('name') }}" placeholder="naam" required>
+           <small class="form-text text-muted" id="emailHelp">*verplicht</small>
+           <small class="text-danger"></small>
+         </div>
+       </div><!-- form-row -->
+     </div><!-- form-group has-warning -->
+         <div class="form-group has-warning">
+           <div class="form-row">
+             <div class="col-sm-12">
+               <label class="form-control-label" for="email">Email*</label>
+               <input class="form-control form-control-danger" id="email" type="email" name="email" value="{{ old('email') }}" placeholder="voorbeeld@voorbeeld.com" aria-describedby="emailHelp" required>
+               <small class="form-text text-muted" id="emailHelp">*verplicht, wij delen uw emailadres niet met anderen</small>
+               <small class="text-danger"></small>
+             </div>
+           </div><!-- form-row -->
+         </div><!-- form-group has-warning -->
+         
+         {{-- wordt nog toegevoegd <div class="form-group has-warning">
+           <label class="form-control-label" for="subject" name="subject" required>Onderwerp*</label>
+           <input class="form-control form-control-danger" name="subject" type="text" placeholder="onderwerp" value="{{ old('subject') }}" required>
+           <small class="form-text text-muted" id="emailHelp">*verplicht</small>
+           <small class="text-danger"></small>
+         </div>--}}
+         <div class="form-group has-warning">
+           <label class="form-control-label" for="message" name="message">Bericht*</label>
+           <textarea name="message" id="text" cols="30" rows="10" class="form-control form-control-danger" placeholder="laat een bericht achter" required>{{ old('message') }}</textarea>
+           <small class="form-text text-muted" id="emailHelp">*verplicht</small>
+           <small class="text-danger"></small>
+         </div>
+   </fieldset>
+   <button class="btn btn-primary text-white" type="submit">Verzenden</button>
+ </form>
+ </div><!-- container p-5 -->
+
+{{--<main class="py-5">
     <h1 style="margin: 0% 10%;">Laat een bericht achter</h1><br>
     @csrf
     {!!Form::open(['action'=> 'ContactUsController@contactUsPost', 'method' => 'POST'])!!}
@@ -24,71 +68,7 @@
         
 
     {!! Form::close() !!}
-</main>
+</main>--}}
     
 @endsection
 
-{{--<div class="container p-5" id="contact-form">
-    <h2 class="text-center">Heeft u een vraag?</h2>
- <form method="POST" action="/contacts">
-   @csrf
-   <fieldset>
-     @if (Auth::user())
-     <div class="form-group has-warning">
-       <div class="form-row">
-         <div class="col-sm-6">
-           <label class="form-control-label" for="fname">Voornaam*</label>
-           <input type="hidden" name="user_id" value="{{ Auth::user()->user_id }}">
-           <input name="fname" class="form-control form-control-danger" id="fname" type="text" value="{{ Auth::user()->voornaam }}" placeholder="voornaam">
-           <small class="text-danger"></small>
-         </div>
-         <div class="col-sm-6">
-           <small class="text-muted float-right">* is verplicht</small>
-           <label class="form-control-label" for="lname">Achternaam*</label>
-           <input name="lname" class="form-control form-control-danger" id="lname" type="text" value="{{ Auth::user()->achternaam }}" placeholder="achternaam">
-           <small class="text-danger"></small>
-         </div>
-       </div><!-- form-row -->
-     </div><!-- form-group has-warning -->
-         <div class="form-group has-warning">
-           <div class="form-row">
-             <div class="col-sm-6">
-               <label class="form-control-label" for="email">Email*</label>
-               <input class="form-control form-control-danger" id="email" type="email" name="email" value="{{ Auth::user()->email }}" placeholder="emailadres" aria-describedby="emailHelp">
-               <small class="form-text text-muted" id="emailHelp">Wij delen uw emailadres niet met anderen</small>
-               <small class="text-danger"></small>
-             </div>
-             <div class="col-sm-6">
-               <label class="form-control-label" for="phone">Telefoonnummer*</label>
-               <input class="form-control form-control-danger" id="phone" type="tel" name="email" value="{{ Auth::user()->telefoon }}" placeholder="telefoonnummer" aria-describedby="emailHelp">
-               <small class="text-danger"></small>
-             </div>
-           </div><!-- form-row -->
-         </div><!-- form-group has-warning -->
-         <div class="form-group has-warning">
-           <label class="form-control-label" for="onderwerp" name="onderwerp" required>Onderwerp*</label>
-           <input class="form-control form-control-danger" name="onderwerp" type="text" placeholder="onderwerp" value="{{ old('onderwerp') }}">
-           <small class="text-danger"></small>
-         </div>
-         <div class="form-group has-warning">
-           <label class="form-control-label" for="bericht" name="bericht">Bericht*</label>
-           <textarea name="bericht" id="text" cols="30" rows="10" class="form-control form-control-danger" placeholder="laat een bericht achter" required>{{ old('bericht') }}</textarea>
-           <small class="text-danger"></small>
-         </div>
-       @endif
-       @if (Auth::guest())
-       <div class="form-group has-warning">
-         <label class="form-control-label" for="onderwerp" name="onderwerp" required>Onderwerp*</label>
-         <input class="form-control form-control-danger" name="onderwerp" type="text" placeholder="onderwerp" value="{{ old('onderwerp') }}">
-         <small class="text-danger"></small>
-       </div>
-       <div class="form-group has-warning">
-         <label class="form-control-label" for="bericht" name="bericht">Bericht*</label>
-         <textarea name="bericht" id="text" cols="30" rows="10" class="form-control form-control-danger" placeholder="laat een bericht achter" required>{{ old('bericht') }}</textarea>
-         <small class="text-danger"></small>
-       </div>
-     @endif
-   </fieldset>
-   <button class="btn btn-orange text-light" type="submit">Verstuur</button>
- </form>
- </div><!-- container p-5 -->--}}
